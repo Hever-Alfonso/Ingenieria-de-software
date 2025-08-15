4379557de59e2df5f1a8b6472d7af72a99860874
Hever Alfonso <haalfonsoj@eafit.edu.co>
2025-08-15 16:53:07 -0500

    Bloque A: App experiences registrada con URLs y vista mínima funcionando (PoC hello world)

--- poc/askmejobs/settings.py
+++ poc/askmejobs/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # App del proyecto
+    'experiences',
 ]
 
 MIDDLEWARE = [
